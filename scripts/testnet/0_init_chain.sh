b53cd293465623e605b05a7349be9f6ed7225647
Alan <hyperalan@protonmail.com>
2024-03-01 10:13:12 -0800

    new tesnet genesis

--- scripts/testnet/0_init_chain.sh
+++ scripts/testnet/0_init_chain.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 superstructd init superstruct-1 --chain-id hyper-1 -o
+
+cp ~/.superstructd/config/genesis.json ./networks/testnet-1/genesis.json
--- scripts/testnet/_reset_chain.sh
+++ scripts/testnet/_reset_chain.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 superstructd tendermint unsafe-reset-all
+
+
