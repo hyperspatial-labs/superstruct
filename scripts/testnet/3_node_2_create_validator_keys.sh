2b43b29efe2b3b7f22df94c00d4a864206e1cd48
Alan <hyperalan@protonmail.com>
2024-03-01 00:32:46 -0800

    ...

--- scripts/testnet/3_node_2_create_validator_keys.sh
+++ scripts/testnet/3_node_2_create_validator_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # on node/validator 2 (awspp)
-cp ./testnet/genesis.json ~/.superstructd/config/ && superstructd keys add validator2
+cp ./networks/testnet/genesis.json ~/.superstructd/config/ && superstructd keys add validator2
